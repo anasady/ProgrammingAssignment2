95d76e3800906e395ac4cb86a26cf79b1a15d598
Ana Sady-Dubon <asady-dubon@Mac-acbc327a58cf.fios-router.home>
2015-08-17 22:10:07 -0400

    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 ## Create helper functions used to store the current value
 ## and the inverse of the value
 makeCacheMatrix <- function(val = matrix()) {
+  ## set the inverse value to null
   inv <- NULL
+  
+  ## used to set the current value
   set <- function(y) {
     val <<- y
     inv <<- NULL
   }
+  
+  ## return the current value
   get <- function() val
+  
+  ## set the inverse value
   setinverse <- function(inverse) inv <<- inverse
+  
+  ##return the inverse value
   getinverse <- function() inv
+  
+  ## provide access to all the functions
   list(
     set = set, 
     get = get,
@@ -21,15 +31,15 @@ makeCacheMatrix <- function(val = matrix()) {
   )
 }
 
-
-## Write a short comment describing this function
 ## get the inverse of the matrix
 ## if the value is null, then calculate the inverse
 ## cache the inverse value
 cacheSolve <- function(x, ...) {
+  
   ## Return a matrix that is the inverse of 'x'
   ## Attempt to retrive the inverse
   inv <- x$getinverse()
+  
   ## If the cached value exists, return cached value
   if(!is.null(inv)) {
     message("getting cached data")
